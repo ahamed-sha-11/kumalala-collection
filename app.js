428182367df055bc235b978bc76b03707a6cdcdc
ahamed sha <101658457+ahamed-sha-11@users.noreply.github.com>
2023-06-25 11:52:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,14 +5,18 @@ const nav = document.getElementById('navbar');
 if(bar)
 {
     bar.addEventListener('click',()=> {
-        nav.classList.add('active');
+        nav.style.display="flex";
+        const myTimeout = setTimeout(()=>nav.classList.add('active'), 50);
+        
     })
 }
 
 if(close)
 {
     close.addEventListener('click',()=> {
-        nav.style.display="flex"
+        
         nav.classList.remove('active');
+        const myTimeout = setTimeout(()=>nav.style.display="none", 50);
+        
     })
 }
